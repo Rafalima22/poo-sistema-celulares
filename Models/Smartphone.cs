19364705411d0739a3a6a53bc6e6526e957292ce
Rafael Rodrigues <rafaelrlima90@hotmail.com>
2025-10-29 01:58:21 -0300

    Implementação completa do sistema smartphones

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,14 +3,35 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        //Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+            //Passar os parâmetros do construtor para as propriedades
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
-
+        public void ModeloCelular()
+        {
+            Console.WriteLine($"Modelo: {Modelo}");
+        }
+        public void NumeroCelular()
+        {
+            Console.WriteLine($"Número: {Numero}");
+        }
+        public void IMEICelular()
+        {
+            Console.WriteLine($"IMEI: {IMEI}");
+        }
+        public void MemoriaCelular()
+        {
+            Console.WriteLine($"Memória: {Memoria} GB");
+        }
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("34628", "Nokia Flip", "121212", 64);
+nokia.ModeloCelular();
+nokia.MemoriaCelular();
+nokia.NumeroCelular();
+nokia.IMEICelular();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("99854", "Iphone 16", "121415", 64);
+iphone.ModeloCelular();
+iphone.MemoriaCelular();
+iphone.NumeroCelular();
+iphone.IMEICelular();
+iphone.InstalarAplicativo("Instagram");
+
+
